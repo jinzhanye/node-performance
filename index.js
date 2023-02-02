2cca0616fc8fc139e78af687a28cc6d71ee39886
yejinzhan <jinzhan.ye@zhenai.com>
2023-02-02 18:03:10 +0800

    update

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ app.use(async ctx => {
   console.log('已响应');
 });
 
-app.listen(3000);
+// chrome不同的tab打开同一个url的时候，每个tab会等待前面一个tab执行完才会执行。不能并发，而 firefox 不会有这个问题
 
 async function getResult(){
   return new Promise( resolve => {
-    setTimeout( resolve, 5000, {
+    setTimeout( resolve, 3000, {
       data: 'hello, world'
     });
   })
 }
+
+app.listen(3011, () => {
+  console.log('listening on http://localhost:3011')
+});
+
